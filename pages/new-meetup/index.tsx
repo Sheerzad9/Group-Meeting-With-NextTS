958875e33818629f09c45e0ab0baa2f263e09789
Ali Sheerzad <ali.sheerzad9@gmail.com>
2023-08-03 19:46:34 +0300

    Added logic to save new meetup in db

--- pages/new-meetup/index.tsx
+++ pages/new-meetup/index.tsx
@@ -1,8 +1,17 @@
+import axios from "axios";
+import { useRouter } from "next/router";
 import NewMeetupForm from "@/components/meetups/NewMeetupForm";
 
 export default function NewMeetUpPage() {
-  function addMeetUpHandler(enteredMeetUpData: any) {
-    console.log(enteredMeetUpData);
+  const router = useRouter();
+
+  async function addMeetUpHandler(enteredMeetUpData: any) {
+    const res = await axios.post("/api/new-meetup", enteredMeetUpData);
+
+    const { data } = res;
+    console.log(data);
+
+    router.push("/");
   }
 
   return <NewMeetupForm onAddMeetup={addMeetUpHandler} />;
