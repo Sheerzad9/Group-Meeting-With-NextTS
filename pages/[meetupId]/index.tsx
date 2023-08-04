866b661ec7e797e63a8c3588ba6e86960d82e214
Ali Sheerzad <ali.sheerzad9@gmail.com>
2023-08-04 17:42:55 +0300

    Checking props values, otherwise there will be type-error in build phase

--- pages/[meetupId]/index.tsx
+++ pages/[meetupId]/index.tsx
@@ -8,10 +8,10 @@ import { ParsedUrlQuery } from "querystring";
 export default function MeetupDetails(props: { meetUpData: Meetup }) {
   return (
     <MeetUpDetail
-      image={props.meetUpData.image}
-      title={props.meetUpData.title}
-      address={props.meetUpData.address}
-      description={props.meetUpData.description}
+      image={props?.meetUpData?.image}
+      title={props?.meetUpData?.title}
+      address={props?.meetUpData?.address}
+      description={props?.meetUpData?.description}
     />
   );
 }
@@ -19,7 +19,13 @@ export default function MeetupDetails(props: { meetUpData: Meetup }) {
 export const getStaticPaths: GetStaticPaths = async () => {
   return {
     fallback: true, // If set to false, this tells next.js that all possible paths are listed in 'path' key value, so if you enter params other than found in the list -> 404 ERROR. So if you set fallback to true, next tries to create the page even tho the param is not found in the paths key value list.
-    paths: [],
+    paths: [
+      {
+        params: {
+          meetupId: "64cbc8409cc8c7ebe930d9aa",
+        },
+      },
+    ],
   };
 };
 
